2f99b378fd60b622c1a6e732276206a37f0fb5f9
SimoG77 <simone.guida@studenti.unina.it>
2026-06-20 13:41:54 +0200

    classe ConnessioneDatabase
    Aggiunta classe ConnessioneDatabase per la gestione della connessione PostgreSQL.

--- src/main/java/database_connection/ConnessioneDatabase.java
+++ src/main/java/database_connection/ConnessioneDatabase.java
@@ -1,5 +1,30 @@
 package database_connection;
 
-public class ConnessioneDatabase {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+    public class ConnessioneDatabase {
+    private static final String URL = "jdbc:postgresql://localhost:5432/Gruppo26";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Simo26";
+
+    private static Connection connection = null;
+
+    private ConnessioneDatabase() {}
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                System.out.println("Connessione al database stabilita con successo.");
+            } catch (ClassNotFoundException | SQLException e) {
+                System.err.println("Errore di connessione al database: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
 }
