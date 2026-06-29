71e33574480482be38de44ccc9f014e39dffc256
SimoG77 <simone.guida@studenti.unina.it>
2026-06-29 14:16:39 +0200

    Aggiunta interfaccia LezioneDao
    Aggiunta interfaccia LezioneDAO per gestire le operazioni di salvataggio e recupero delle lezioni sul database.

--- src/main/java/dao/LezioneDAO.java
+++ src/main/java/dao/LezioneDAO.java
@@ -1,9 +1,26 @@
 package dao;
 
 import model.Lezione;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
 
 public interface LezioneDAO {
-    List<Lezione> getAllLezioni();
-    boolean aggiornaLezione(Lezione lezione);
+
+    List<Lezione> trovaTutte();
+
+    List<Lezione> trovaPerGiorno(LocalDate giorno);
+
+    List<Lezione> trovaPerAula(String nomeAula);
+
+    List<Lezione> trovaPerDocente(String loginDocente);
+
+    Optional<Lezione> trovaPerID(int id);
+
+    boolean inserisci(Lezione lezione);
+
+    boolean aggiorna(int id, Lezione lezione);
+
+    boolean elimina(int id);
 }
