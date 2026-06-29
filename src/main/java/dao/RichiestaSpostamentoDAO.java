d683ebbe90162fa5a84a4648f92cb2156681ee66
SimoG77 <simone.guida@studenti.unina.it>
2026-06-29 14:17:36 +0200

    Aggiunta interfaccia RichiestaSpostamentoDAO
    Aggiunta interfaccia RichiestaSpostamentoDAO per gestire il salvataggio e lo stato delle richieste di spostamento nel database.

--- src/main/java/dao/RichiestaSpostamentoDAO.java
+++ src/main/java/dao/RichiestaSpostamentoDAO.java
@@ -1,8 +1,24 @@
 package dao;
 
 import model.RichiestaSpostamento;
+import model.RichiestaSpostamento.StatoRichiesta;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface RichiestaSpostamentoDAO {
-    boolean inserisciRichiesta(RichiestaSpostamento richiesta);
-    boolean aggiornaStatoRichiesta(RichiestaSpostamento richiesta);
+
+    List<RichiestaSpostamento> trovaTutte();
+
+    List<RichiestaSpostamento> trovaPerDocente(String loginDocente);
+
+    List<RichiestaSpostamento> trovaPerStato(StatoRichiesta stato);
+
+    Optional<RichiestaSpostamento> trovaPerID(int id);
+
+    boolean inserisci(RichiestaSpostamento richiesta);
+
+    boolean aggiornaStato(int id, StatoRichiesta stato);
+
+    boolean elimina(int id);
 }
