f37f116abd9b098806de19fd52d625f66119ab37
SimoG77 <simone.guida@studenti.unina.it>
2026-06-30 23:29:42 +0200

    Modificata classe Docente
    FIX: rimossi i campi "insegnamenti" e "lezioni" che erano dichiarati ma mai letti/scritti da nessun metodo (codice morto),
    aggiunta la lista reale dei Vincolo del docente
    Applica  la regola di dominio "massimo 3 vincoli per docente"
    rinominato da "richiedispostamento" a "richiediSpostamento"

--- src/main/java/model/Docente.java
+++ src/main/java/model/Docente.java
@@ -6,12 +6,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Docente extends Utente {
-    private List<Insegnamento> insegnamenti =new ArrayList<Insegnamento>();
-    private List<Lezione> lezioni= new ArrayList<>();
+    private List<Vincolo> vincoli = new ArrayList<>();
+
     public Docente(String nome, String cognome, String email, String login, String password) {
-        super(nome,cognome,email,login,password);
+        super(nome, cognome, email, login, password);
     }
-    public void richiedispostamento(Lezione lezione, LocalTime nuovaOraInizio, LocalTime nuovaOraFine, LocalDate nuovoGiorno, Responsabile responsabile) {
+
+    public List<Vincolo> getVincoli() {
+        return vincoli;
+    }
+
+    public void setVincoli(List<Vincolo> vincoli) {
+        this.vincoli = (vincoli != null) ? vincoli : new ArrayList<>();
+    }
+
+    public boolean aggiungiVincolo(Vincolo vincolo) {
+        if (this.vincoli.size() >= 3) {
+            System.out.println("Impossibile aggiungere vincolo: limite massimo di 3 raggiunto per " + cognome);
+            return false;
+        }
+        this.vincoli.add(vincolo);
+        return true;
+    }
+
+    public void richiediSpostamento(Lezione lezione, LocalTime nuovaOraInizio, LocalTime nuovaOraFine, LocalDate nuovoGiorno, Responsabile responsabile) {
         RichiestaSpostamento nuovaRichiesta = new RichiestaSpostamento(nuovaOraInizio, nuovaOraFine, nuovoGiorno, lezione);
         responsabile.riceviRichiesta(nuovaRichiesta);
         System.out.println("Richiesta di spostamento inviata al responsabile per la lezione del " + nuovoGiorno);
